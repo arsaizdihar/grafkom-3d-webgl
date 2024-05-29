7cde9c336bb4231eb13cfd90f90ebe84e0d2a667
arsaizdihar <arsadihar@gmail.com>
2024-05-29 09:07:29 +0700

    fix: remove unused files

--- src/lib/light/directional-light-shadow.ts
+++
@@ -1,9 +0,0 @@
-import { OrthographicCamera } from "../camera/orthographic-camera";
-import { Color } from "../engine/color";
-import { LightShadow } from "../engine/light-shadow";
-
-export class DirectionalLightShadow extends LightShadow {
-  constructor() {
-    super(new OrthographicCamera(-5, 5, 5, -5, 0.5, 500));
-  }  
-}
